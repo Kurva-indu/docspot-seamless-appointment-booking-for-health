9ef8098d654bd7730ecff7d572f11b29bbf8acab
Kurva-indu <162704816+Kurva-indu@users.noreply.github.com>
2025-06-26 19:57:38 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="container mt-5">
+      <h1>Welcome to DocSpot</h1>
+      <p>Book appointments easily with doctors!</p>
+    </div>
+  );
+}
+
+export default Home;
