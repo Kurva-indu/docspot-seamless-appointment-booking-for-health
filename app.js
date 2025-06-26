293f2018ac79edf24b108ef321910fd5eb5964df
Kurva-indu <162704816+Kurva-indu@users.noreply.github.com>
2025-06-26 20:00:00 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+import UserDashboard from "./pages/UserDashboard";
+import DoctorDashboard from "./pages/DoctorDashboard";
+
+
+<Route path="/dashboard" element={<UserDashboard />} />
+<Route path="/doctor-dashboard" element={<DoctorDashboard />} />
